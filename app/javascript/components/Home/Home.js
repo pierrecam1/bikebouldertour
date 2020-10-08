418bd59a064d0e2e7109218efe5df24c463dec42
C <pierrecammas31470@gmail.com>
2020-10-08 16:29:09 +0200

    Finish home header and add Flickity

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import Countdown from 'react-countdown';
+import Button from '../UI/Button/Button'
 
 const renderer =({days, hours, minutes, seconds, completed}) =>{
     if(completed){
@@ -9,16 +10,16 @@ const renderer =({days, hours, minutes, seconds, completed}) =>{
     }
     if(days > 0) {
         return (
-            <h2 className="d-flex">
-                <div className="d-flex flex-column">
+            <h2 className="d-flex justify-content-center counter">
+                <div className="d-flex flex-column col-4">
                     {days}
                     <span>Days</span>
                 </div>
-                <div className="d-flex flex-column">
+                <div className="d-flex flex-column col-4">
                     {hours} 
                     <span>Hours</span>
                 </div>
-                <div className="d-flex flex-column">
+                <div className="d-flex flex-column col-4">
                     {minutes} 
                     <span>Minutes</span>
                 </div>
@@ -27,7 +28,7 @@ const renderer =({days, hours, minutes, seconds, completed}) =>{
     }
     else{
         return (
-            <h2 className="d-flex">
+            <h2 className="d-flex justify-content-between counter">
                 <div className="d-flex flex-column">
                     {hours} 
                     <span>Hours</span>
@@ -48,17 +49,28 @@ const home = () => {
     return(
         <Fragment>
             <header className="bg-dark">
-            <section className="container text-white">
-                <h1>Bike boulder tour</h1>
-                <hr/>
-                <Countdown date={new Date("Apr 1, 2021 00:00:00")} renderer={renderer} />
-                <hr/>
-                <h3>Comming Soon</h3>
-                <p>If you have a slogan, you could be sponsoring this great event. 
-                    The only thing you have to do, is to submit the form
-                </p>
+            <section className="container justify-content-center text-white d-flex align-items-center">
+                <div className="card bg-blur col-sm-12 col-md-8 col-lg-6 text-center p-5">
+                    <h1>Bike boulder tour</h1>
+                    <hr/>
+                    <Countdown date={new Date("Apr 1, 2021 00:00:00")} renderer={renderer} />
+                    <hr/>
+                    <h3>Comming Soon</h3>
+                    <p className="w-75 mx-auto">If you have a slogan, you could be sponsoring this great event. 
+                        The only thing you have to do, is to submit the form
+                    </p>
+                    <Button>Ask for Sponsorship</Button>
+                </div>
+
             </section>
             </header>
+            <section className="bg-dark text-white">
+                <div className="container">
+                    <h2>LIST OF RIDERS</h2>
+                    <div className="card">
+                </div>
+                </div>
+            </section>
         </Fragment>
     )
 };
---
+++ app/javascript/components/UI/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { btn } from './Button.module.scss';
+const Button = (props) => {
+  return (
+      <div>
+        <button
+        className={btn}
+        onClick={() => alert('I am styled with SCSS Modules')}>
+        {props.children}
+        </button>
+      </div>
+
+    
+  );
+};
+export default Button;
