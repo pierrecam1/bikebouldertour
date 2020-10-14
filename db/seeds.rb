3b698ff40875a0d23591ee0f42ec1d863837d581
C <pierrecammas31470@gmail.com>
2020-10-14 11:43:35 +0200

    Correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ bikers = Biker.create([
         first_name:"Dustin",
         last_name:"Green",
         city:"Hickory Hills",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602664525/boulderbiketour/Dustin-Green_to013h.png",
         state:"IL",
         latitude:"40.00",
         longitude:"-105.35"
@@ -20,7 +20,7 @@ bikers = Biker.create([
         first_name:"Jason",
         last_name:"Finn",
         city:"Huntington Beach",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602665683/boulderbiketour/Jason-Finn_tbplgt.png",
         state:"CA",
         latitude:"39.95",
         longitude:"-105.24"
@@ -29,7 +29,7 @@ bikers = Biker.create([
         first_name:"Howard",
         last_name:"Thompson",
         city:"Hale",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602664525/boulderbiketour/Howard-Thompson_etpizc.png",
         state:"MO",
         latitude:"40.06",
         longitude:"-105.26"
@@ -38,7 +38,7 @@ bikers = Biker.create([
         first_name:"Maggie",
         last_name:"Lantz",
         city:"Dublin",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602664525/boulderbiketour/Maggie-Lantz_olw6xc.png",
         state:"OH",
         latitude:"40.03",
         longitude:"-105.23"
@@ -47,7 +47,7 @@ bikers = Biker.create([
         first_name:"Lawrence",
         last_name:"Duran",
         city:"Fort Myers",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602665526/boulderbiketour/Lawrence-Duran_rhjn9k.png",
         state:"FL",
         latitude:"40.04",
         longitude:"-105.23"
@@ -56,7 +56,7 @@ bikers = Biker.create([
         first_name:"Irene",
         last_name:"Molina",
         city:"Tucson",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667877/boulderbiketour/Irene-Molina_y5natq.png",
         state:"AZ",
         latitude:"39.96",
         longitude:"-105.22"
@@ -65,7 +65,7 @@ bikers = Biker.create([
         first_name:"Nancy",
         last_name:"Garner",
         city:"Baltimore",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Nancy-Garner_owvpbj.png",
         state:"MD",
         latitude:"39.98",
         longitude:"-105.21"
@@ -74,7 +74,7 @@ bikers = Biker.create([
         first_name:"Tara",
         last_name:"Taylor",
         city:"Minneapolis",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Tara-Taylor_pkasgq.png",
         state:"MN",
         latitude:" 40.00",
         longitude:"-105.25"
@@ -83,7 +83,7 @@ bikers = Biker.create([
         first_name:"Alejandro",
         last_name:"Smith",
         city:"Scarborough",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Alejandro-Smith_azeuvx.png",
         state:"ME",
         latitude:"40.02",
         longitude:"-105.26"
@@ -92,7 +92,7 @@ bikers = Biker.create([
         first_name:"Tricia",
         last_name:"Renshaw",
         city:"Mystic",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Trica-Renshaw_weaj08.png",
         state:"CT",
         latitude:"40.02",
         longitude:"-105.30"
@@ -101,7 +101,7 @@ bikers = Biker.create([
         first_name:"Travis",
         last_name:"Cook",
         city:"Twin Lakes",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Travis-Cook_eb4xdw.png",
         state:"WI",
         latitude:"40.01",
         longitude:"-105.20"
@@ -110,7 +110,7 @@ bikers = Biker.create([
         first_name:"Joan",
         last_name:"Brooks",
         city:"Norfolk",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Joan-Brooks_h6qkmw.png",
         state:"VA",
         latitude:"39.98",
         longitude:" -105.24"
@@ -119,7 +119,7 @@ bikers = Biker.create([
         first_name:"Joseph",
         last_name:"Rodgers",
         city:"Wayne",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Joseph-Rodgers_nb1uox.png",
         state:"NJ",
         latitude:" 39.99",
         longitude:"-105.25"
@@ -128,7 +128,7 @@ bikers = Biker.create([
         first_name:"Matthew",
         last_name:"Gregson",
         city:"Brashear",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Matthew-Gregson_zl8ao2.png",
         state:"MO",
         latitude:" 40.01",
         longitude:" -105.29"
@@ -137,7 +137,7 @@ bikers = Biker.create([
         first_name:"Katie",
         last_name:"Dunlap",
         city:"Marion",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Katie-Dunlap_xwkco1.png",
         state:"VA",
         latitude:"39.97",
         longitude:"-105.27"
@@ -146,7 +146,7 @@ bikers = Biker.create([
         first_name:"Leo",
         last_name:"Howard",
         city:"Dallas",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667878/boulderbiketour/Leo-Howard_yir3mk.png",
         state:"TX",
         latitude:"39.97",
         longitude:"-105.33"
@@ -155,7 +155,7 @@ bikers = Biker.create([
         first_name:"Maria",
         last_name:"Baisden",
         city:"Raleigh",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667877/boulderbiketour/Maria-Baisden_f0jn60.png",
         state:"NC",
         latitude:"40.00",
         longitude:"-105.29"
@@ -164,7 +164,7 @@ bikers = Biker.create([
         first_name:"Sandy",
         last_name:"Townsend",
         city:"Newport",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667877/boulderbiketour/Sandy-Townsend_co066h.png",
         state:"PA",
         latitude:"40.05",
         longitude:" -105.30"
@@ -173,7 +173,7 @@ bikers = Biker.create([
         first_name:"Melinda",
         last_name:"Stephenson",
         city:"Kahului",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667877/boulderbiketour/Melinda-Stephenson_bhzt20.png",
         state:"HI",
         latitude:"39.99",
         longitude:" -105.31"
@@ -182,7 +182,7 @@ bikers = Biker.create([
         first_name:"Jason",
         last_name:"Nichols",
         city:"Alexandria",
-        avatar:"app/assets/images/Dustin-Green.png",
+        avatar:"https://res.cloudinary.com/derbbp44x/image/upload/v1602667877/boulderbiketour/Jason-Nichols_facu62.png",
         state:"VA",
         latitude:"40.01",
         longitude:" -105.32"
