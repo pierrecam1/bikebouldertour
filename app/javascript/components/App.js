8f1667834d5cb8032181dc9de8916258d6bdcc13
C <pierrecammas31470@gmail.com>
2020-11-02 09:30:50 +0100

    comit after merge

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,8 +4,12 @@ import Home from './Home/Home';
 import Bikers from './Bikers/Bikers'
 import Localize from './Localize/Localize';
 import Photos from './Photos/Photos';
+<<<<<<< HEAD
 import SponsoringForm from './Sponsoring/SponsoringForm';
 import SponsoringFormTest from './Sponsoring/SponsoringFormTest';
+=======
+import Sponsoring from './Sponsoring/Sponsoring';
+>>>>>>> parent of 1f0ebda... Add SponsoringForm
 
 
 
@@ -16,8 +20,12 @@ const App = () => {
             <Route exact path="/localize" component={Localize} />
             <Route exact path="/bikers" component={Bikers} />
             <Route exact path="/photos" component={Photos} />
+<<<<<<< HEAD
             <Route exact path="/sponsoring" component={SponsoringForm} />
             <Route exact path="/sponsoring-test" component={SponsoringFormTest} />
+=======
+            <Route exact path="/sponsoring" component={Sponsoring} />
+>>>>>>> parent of 1f0ebda... Add SponsoringForm
         </Switch>
     )
 };
